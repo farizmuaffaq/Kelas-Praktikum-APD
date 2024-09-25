2f2bc43f1cb505b453786ef0e21794cb1dbead7b
Justfarizz <farizmuaffaq@gmail.com>
2024-09-25 09:05:00 +0800

    Upload Codingan Pertemuan 4

---
+++ Pertemuan-4/2409106032_FarizMuwaffaq_A2-24.py
@@ -0,0 +1,80 @@
+# for j in range(2,12,2):
+#     print ("halo")
+# print("hai")
+
+# print("Menu Rumah Makan Informatika 24: ")
+# print("--------------------------------")
+# simpan = ["Nasi Goreng", "Mie Goreng", "Mie Ayam", "Bakso", "Soto"]
+# for i in simpan:
+#     print(i)
+
+# print("Menu Rumah Makan Informatika 24: ")
+# print("--------------------------------")
+# simpan = ["Nasi Goreng", "Mie Goreng", "Mie Ayam", "Bakso", "Soto"]
+# for i, menu in enumerate(simpan,start=1):
+#     print(f"{i}.{menu}")
+
+# print("Menu Rumah Makan Informatika 24: ")
+# print("--------------------------------")
+# simpan = ["Nasi Goreng", "Mie Goreng", "Mie Ayam", "Bakso", "Soto"]
+# for i in range(len(simpan)):
+#     print(f"{i+1}. {simpan[i]}")
+
+# makanan = ["mie","sop","bakso"]
+# minuman = ["es teh","air putih","es jeruk"]
+
+# for i in makanan:
+#     for j in minuman:
+#         print(f"{i} & {j}")
+
+# jawab = 'ya'
+# hitung = 0
+# while(jawab == 'ya'):
+#     hitung += 1
+#     jawab = input("Ulang lagi tidak? ")
+# print(f"Total perulangan: {hitung}")
+
+# i = 0
+# while i < 5:
+#     print (i)
+#     i += 1
+
+# i = 0
+# while i < 5:
+#     print (i)
+#     break
+#     i += 1
+
+# hitung = 0
+# while True:
+#     hitung += 1
+#     ulang = input("Masih Ingin Mengulang? ")
+#     if ulang == "tidak" or ulang =="Tidak":
+#         print("Oke kita udahan")
+#         break
+
+# print(f"Total Perulangan: {hitung}")
+
+# hitung = 0
+# while True:
+#     hitung += 1
+#     ulang = input("Masih Ingin Mengulang? ")
+#     if ulang == "y" or ulang =="Y":
+#         print("Oke kita lanjut")
+#         continue
+
+# print(f"Total Perulangan: {hitung}")
+
+# print(“Daftar bilangan ganjil dari 1-10”)
+# for i in range(10):
+#     if i % 2 == 0:
+#         continue
+# print(i)
+
+# total = 0
+# while True:
+#     angka = int(input("masukkan angka positif (input negatif jika ingin berhenti):"))
+#     if angka < 0:
+#         break
+#     total += angka
+# print("jumlah total adalah ", total)
